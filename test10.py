c0a717526715557c53f399624461576feab9a139
zola tsai <zolazeize@gmail.com>
2021-01-29 10:47:14 +0800

    0129練習

---
+++ test10.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+      
+img = cv2.imread('2021012516112623.jpg',1)
+b = cv2.imread('2021012516112623.jpg',1)
+e1 = cv2.getTickCount()
+img1 = b[600:700,100:300]
+img2 = b[100:200,700:900]
+print(img1)
+print(img2)
+dst=cv2.addWeighted(img1,0.1,img2,0.9,0)
+b[100:200,700:900]=dst
+e2=cv2.getTickCount()
+t = (e2-e1) / cv2.getTickFrequency()
+print(t)
+cv2.namedWindow("2021012516112623",cv2.WINDOW_NORMAL)
+cv2.imshow('2021012516112623',img)
+cv2.imshow('b',b)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ test11.py
@@ -0,0 +1,5 @@
+import cv2
+flags=[i for i in dir(cv2) if i startswith)'color']
+print flags
+
+
