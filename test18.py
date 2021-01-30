cbc3ba80751f24d20f72a2edbf3271ff94a6ef5b
zola tsai <zolazeize@gmail.com>
2021-01-30 11:35:35 +0800

    練習至尋找輪廓

---
+++ test18.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+src = cv2.imread('test21.jpg')
+img = src.copy()
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+print(len(contours))
+#cnt = contours[0]
+#print(cnt)
+draw = cv2.drawContours(img, contours, 2, (255, 0, 0), 10)
+draw = cv2.drawContours(img, contours, 1, (0, 255, 0), 10)
+
+plt.subplot(121), plt.imshow(cv2.cvtColor(src, cv2.COLOR_BGR2RGB)), plt.title('Src')
+plt.subplot(122), plt.imshow(cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)), plt.title('image')
+plt.show()
+#寫contours 會把所有邊框畫出來，-1把所有圖案畫出來  
